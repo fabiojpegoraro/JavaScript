e329f29df0f5d5dc65cbbfbb954126b61f091b39
Fábio José Pegoraro <fabiojpegoraro@MacBook-Pro-de-Fabio.local>
2018-10-03 22:40:47 -0300

    templateString e Boolean

---
+++ Exercicios-js/fundamentos/templateString.js
@@ -0,0 +1,13 @@
+const nome = 'Juliana';
+const concat = 'Olá ' + nome + '!';
+const template = `
+Olá
+${nome}!`;
+
+console.log(concat, template);
+
+console.log(`1 + 1 = ${1 + 1}`);
+
+const up = texto => texto.toUpperCase();
+
+console.log(`Ei... ${up('cuidado')}`);
