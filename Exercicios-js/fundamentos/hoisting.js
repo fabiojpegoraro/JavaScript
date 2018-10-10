4f5bfdfa047c88c8a7f18b250b6f8ad184d94ba0
Fábio José Pegoraro <fabiojpegoraro@MacBook-Pro-de-Fabio.local>
2018-10-10 18:38:43 -0300

    alteracoes

---
+++ Exercicios-js/fundamentos/hoisting.js
@@ -0,0 +1,8 @@
+console.log('a=', a)
+var a = 2
+console.log('a=', a)
+
+let b
+console.log('b=', b)
+b = 2
+console.log('b=', b)
---
+++ Exercicios-js/fundamentos/usandoLetEmLoop1.js
@@ -0,0 +1,4 @@
+for(let a = 0; a < 10; a++){
+    console.log(a);
+}
+console.log('valor de a=', a);
